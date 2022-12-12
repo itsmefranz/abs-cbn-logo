57ce46ccb0ed375d10a454eb23eb4a1f058ed6c3
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2022-12-13 04:35:55 +0800

    plotted all the other colors and finished the code

--- script.js
+++ script.js
@@ -2,7 +2,38 @@ const cvs = document.getElementById("canvas");
 const ctx = cvs.getContext("2d");
 
 ctx.beginPath();
-ctx.arc(610, 275, 250, 0.5 * Math.PI, 2.5 * Math.PI);
+ctx.arc(580, 275, 250, 0.5 * Math.PI, 2.5 * Math.PI);
 ctx.fillStyle = "red";
 ctx.fill();
 
+ctx.beginPath();
+ctx.arc(580, 275, 210, 0.5 * Math.PI, 2.5 * Math.PI);
+ctx.fillStyle = "white";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(580, 275, 185, 0.5 * Math.PI, 2.5 * Math.PI);
+ctx.fillStyle = "green";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(580, 275, 150, 0.5 * Math.PI, 2.5 * Math.PI);
+ctx.fillStyle = "white";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(580, 275, 125, 0.5 * Math.PI, 2.5 * Math.PI);
+ctx.fillStyle = "blue";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(580, 275, 90, 0.5 * Math.PI, 2.5 * Math.PI);
+ctx.fillStyle = "white";
+ctx.fill();
+
+ctx.clearRect(550, 270, 60, 400);
+ctx.fillStyle = "black";
+ctx.fillRect(560, 330, 40, 215);
+
+ctx.font = "bold 145px Rotis Semi Serif";
+ctx.fillText("ABS-CBN", 325, 660);
